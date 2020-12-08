cf297be9136f311f7917290e529fd2c78b599ad1
David031 <davidtsang031@gmail.com>
2020-12-08 14:41:39 +0800

    change model to game

--- Assets/Scripts/Level3/Tower_rotation.cs
+++ Assets/Scripts/Level3/Tower_rotation.cs
@@ -6,6 +6,7 @@ public class Tower_rotation : MonoBehaviour {
     public float xSpeed = 0.0f;
     public float ySpeed = 0.0f;
     public float zSpeed = 0.0f;
+    bool isYPlus = false;
     // Start is called before the first frame update
     void Start () {
 
@@ -13,9 +14,24 @@ public class Tower_rotation : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        float ySpeedNow = ySpeed * Time.deltaTime;
+
+        if (ySpeedNow > 20 && isYPlus) {
+            isYPlus = false;
+        } else if (ySpeedNow < -50 && !isYPlus) {
+            isYPlus = true;
+        }
+        if (isYPlus) {
+            ySpeedNow = ySpeed * Time.deltaTime;
+
+        } else {
+            ySpeedNow = -ySpeed * Time.deltaTime;
+        }
+        Debug.Log (isYPlus);
+        Debug.Log (ySpeedNow);
         transform.Rotate (
             xSpeed * Time.deltaTime,
-            ySpeed * Time.deltaTime,
+            ySpeedNow,
             zSpeed * Time.deltaTime
         );
     }
---
+++ Assets/Scripts/hit_detect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Gamekit3D {
+    [RequireComponent (typeof (Collider))]
+    public class hit_detect : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start () {
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+        void OnTriggerEnter (Collider other) {
+            var pc = other.GetComponent<PlayerController> ();
+            if (pc != null) {
+                pc.Die (new Damageable.DamageMessage ());
+            }
+        }
+    }
+}
