4fd53045a6c207bb805ee8c05af874b9ce44b404
David031 <davidtsang031@gmail.com>
2021-05-10 20:34:51 +0800

    add start & end menu

---
+++ Assets/Scripts/SenceChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SenceChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void FinsihLevel()
+    {
+        SceneManager.LoadScene("FinishMenu");
+    }
+    public void ReatartGame()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
