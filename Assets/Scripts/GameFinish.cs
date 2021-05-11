b5e6dc4ddebee3dc21178a5d3c3a49efa18920df
David031 <davidtsang031@gmail.com>
2021-05-11 15:04:25 +0800

    fix gameend bug

---
+++ Assets/Scripts/GameFinish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFinish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Door Open");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("FinishMenu");
+        Debug.Log("Finish");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
