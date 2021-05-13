35a2201415842b630f8f9167e884b3c0a8499547
David031 <davidtsang031@gmail.com>
2021-05-14 04:20:55 +0800

    fix issue

---
+++ Assets/Scripts/together.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class together : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public GameObject plane;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.SetParent(plane.transform);
+        }
+        Debug.Log("Touch");
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.SetParent(null);
+        }
+        Debug.Log("Exit");
+    }
+}
